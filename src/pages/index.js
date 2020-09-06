df8f4e94bf98dd2278353a0eb35edd00ce331ee0
fauzifadillah <fauzifadillah00@yahoo.com>
2020-09-06 14:46:49 +0700

    first

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ import Layout from '../components/layout/Layout';
 import SplitSection from '../components/SplitSection';
 import StatsBox from '../components/StatsBox';
 import customerData from '../data/customer-data';
-import HeroImage from '../svg/HeroImage';
 import SvgCharts from '../svg/SvgCharts';
+import temanwarung from '../svg/temanwarung.png';
 
 export default () => (
   <Layout>
@@ -16,19 +16,19 @@ export default () => (
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-            Main title of your landing page
+            Selalu ada teman
           </h1>
           <p className="text-xl lg:text-2xl mt-6 font-light">
-            Free landing page template to promote your business startup and generate leads for the
-            offered services
+            Social Commerce Platform for Warung
           </p>
-          <p className="mt-8 md:mt-12">
-            <Button size="lg">Get Started</Button>
+          <p className="mt-2 md:mt-12">
+            <Button size="lg">Mulai Sekarang</Button>
           </p>
-          <p className="mt-4 text-gray-600">Sed fermentum felis ut cursu</p>
+          <p className="mt-2 text-gray-600">Sed fermentum felis ut cursu</p>
         </div>
         <div className="lg:w-1/2">
-          <HeroImage />
+          {/* <HeroImage /> */}
+          <img src={temanwarung} alt={temanwarung} />
         </div>
       </div>
     </section>
